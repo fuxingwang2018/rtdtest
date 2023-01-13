11992176e85df4b6494f084305fcf4a9d8e5ede4
Fuxing Wang <fuxing.wang@smhi.se>
2023-01-13 12:40:31 +0100

    Add source_suffix in source/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ extensions = ['sphinx.ext.autodoc']
 templates_path = ['_templates']
 exclude_patterns = []
 
-
+source_suffix = '.rst'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
